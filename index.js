efc232856a16de20df4ef879d74c903bcb0bde12
Matt Rosensweig <matthew.rosensweig@gmail.com>
2020-08-10 17:27:39 -0400

    app functional with placeholder for park address, minimal styling

---
+++ index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const apiKey = 'eleBssoK9XWGnVTT8MyYHtwuzuJ9bQ1L1Y2oYHFf'; 
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+
+function createQueryParameters(query, maxResults, states) {
+  const params = {
+    api_key: apiKey,
+    q: query,
+    stateCode: states,
+    limit: maxResults,
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => {
+        console.log(responseJson)
+        createList(responseJson)
+    })
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function displayList(listHTML) {
+    $('#js-park-list').html(listHTML);
+    $('#list-container').removeClass('hidden');
+}
+
+function createList(responseJson) {
+    let listHTML = "";
+    for (let i=0; i < responseJson.data.length; i++) {
+        listHTML += `<li><h4>${responseJson.data[i].fullName}</h4><ul><li>${responseJson.data[i].description}</li><li><a href="${responseJson.data[i].url}">Website</a></li><li>Address Placeholder</li></ul></li>`;
+    }
+    displayList(listHTML);
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    const maxResults = $('#js-max-results').val();
+    let states = $('#js-states').val();
+    states = states.split(', ');
+    states = states.join();
+    createQueryParameters(searchTerm, maxResults, states);
+  });
+}
+
+$(watchForm);
